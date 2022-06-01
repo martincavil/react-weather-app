510b0b47a58295bdd518180840e80f80031a5feb
Martin Cavil <martincavil.dev@gmail.com>
2022-06-01 16:46:03 +0200

    fixing elements position

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,25 @@ import { useState } from 'react';
 
 function App() {
 
-  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=paris&appid=c0670ccf8896f75507e0281fbdc2b22d';
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c0670ccf8896f75507e0281fbdc2b22d';
   return (
     <div className="app">
        <div className="container">
+
         <div className="top">
           <div className="location">
-            <span>Paris</span>
+            <h2>Paris</h2>
             <div className="temp">
-              <span>28°C</span>
+              <h1>28°C</h1>
             </div>
             <div className="desc">
               <p>Sunny</p>
             </div>
           </div>
         </div>
+
         <div className="bottom">
           <div className="feels">
             <p>30°C</p>
@@ -27,9 +31,10 @@ function App() {
             <p>20%</p>
           </div>
           <div className="wind">
-            <span>12km/h</span>
+            <p>12km/h</p>
           </div>
         </div>
+
       </div>
     </div>
   );
