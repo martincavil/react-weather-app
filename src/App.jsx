b2d671f761c8fa94b70dd0eefaaa25c98ed1e53f
Martin Cavil <martincavil.dev@gmail.com>
2022-06-01 16:10:21 +0200

    import axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './app.scss';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+
+    </div>
   );
 }
 
